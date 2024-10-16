efcaacc333605cbf245fbb2755ac5d4571106d44
devi-priya22 <sdevipriya803@gmail.com>
2024-10-16 23:40:29 +0530

    committed

--- Day12/program.cs
+++ Day12/program.cs
@@ -1,16 +1,13 @@
 //StudentSystem
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Day12_Task1
+namespace Students
 {
+
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var sManager = new StudentManager();
 
@@ -20,30 +17,62 @@ namespace Day12_Task1
                 Console.WriteLine("2. Search");
                 Console.WriteLine("3. Update");
                 Console.WriteLine("4. Delete");
+                Console.WriteLine("5. Add Subject and Marks");
+                Console.WriteLine("6. Exit");
                 var option = Console.ReadLine();
 
                 switch (option)
                 {
                     case "1":
-                        var stud = GetStudentData();
-                        sManager.Add(stud);
+                        var student = GetStudentData();
+                        sManager.Add(student);
                         break;
                     case "2":
-                        Console.Write("Enter the register number to search: ");
-                        var RegNumber = Console.ReadLine();
-                        sManager.Search(RegNumber);
+                        Console.Write("Enter the Reg number to search: ");
+                        var regNo = Console.ReadLine();
+                        sManager.Search(regNo);
                         break;
-                    case "3":
-                        var StudentToUpdate = GetStudentData();
-                        sManager.Update(StudentToUpdate.RegNumber,
-                            StudentToUpdate.Name,
-                            StudentToUpdate.Class);
+                   case "3":
+                        Console.Write("Enter the Reg number to update: ");
+                        var regNumToUpdate = Console.ReadLine();
+                        Console.Write("Enter the new name: ");
+                        var name = Console.ReadLine();
+                        Console.Write("Enter the new standard: ");
+                        var standard = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Subject One: ");
+                        var subjectOne = Console.ReadLine();
+                        Console.Write("Enter Marks for Subject One: ");
+                        var markOne = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Subject Two: ");
+                        var subjectTwo = Console.ReadLine();
+                        Console.Write("Enter Marks for Subject Two: ");
+                        var markTwo = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Subject Three: ");
+                        var subjectThree = Console.ReadLine();
+                        Console.Write("Enter Marks for Subject Three: ");
+                        var markThree = int.Parse(Console.ReadLine());
+
+                        sManager.Update(regNumToUpdate, name, standard, subjectOne, markOne, subjectTwo, markTwo, subjectThree, markThree);
                         break;
                     case "4":
-                        Console.Write("Enter the register number to delete: ");
-                        var RegNumberToDelete = Console.ReadLine();
-                        sManager.Delete(RegNumberToDelete);
+                        Console.Write("Enter the Reg number to delete: ");
+                        var studentNumberToDelete = Console.ReadLine();
+                        sManager.Delete(studentNumberToDelete);
                         break;
+                    case "5":
+                        Console.Write("Enter the Reg number to add subject: ");
+                        var regNumber = Console.ReadLine();
+                        Console.Write("Enter the subject name: ");
+                        var subject = Console.ReadLine();
+                        Console.Write("Enter the marks: ");
+                        var marks = int.Parse(Console.ReadLine());
+                        sManager.AddSubject(regNumber, subject, marks);
+                        break;
+                    case "6":
+                        return;
                     default:
                         Console.WriteLine("Invalid option");
                         break;
@@ -51,48 +80,42 @@ namespace Day12_Task1
             }
         }
 
-        private static Student GetStudentData()
+        private static StudentData GetStudentData()
         {
-            Student stud = new Student();
+            StudentData student = new StudentData();
 
-            Console.Write("Enter the student name: ");
-            stud.Name = Console.ReadLine();
+            Console.Write("Enter the students name: ");
+            student.Name = Console.ReadLine();
 
-            Console.Write("Enter the class: ");
-            stud.Class = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Reg number: ");
+            student.RegNo = Console.ReadLine();
 
-            Console.Write("Enter the register number: ");
-            stud.RegNumber = Console.ReadLine();
+            Console.Write("Enter the standard (1 - 10): ");
+            student.Standard = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Subject1: ");
-            stud.Sub1 = Console.ReadLine();
+            Console.Write("Enter Subject One: ");
+            student.SubjectOne = Console.ReadLine();
 
-            Console.WriteLine("Enter Mark1: ");
-            stud.Mark1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Marks for Subject One: ");
+            student.MarkOne = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Maximum Mark: ");
-            stud.MaxMark1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Subject Two: ");
+            student.SubjectTwo = Console.ReadLine();
 
-            Console.WriteLine("Enter Subject2: ");
-            stud.Sub2 = Console.ReadLine();
+            Console.Write("Enter Marks for Subject Two: ");
+            student.MarkTwo = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Mark2: ");
-            stud.Mark2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Subject Three: ");
+            student.SubjectThree = Console.ReadLine();
 
-            Console.WriteLine("Enter Maximum Mark: ");
-            stud.MaxMark2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Marks for Subject Three: ");
+            student.MarkThree = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Subject3: ");
-            stud.Sub3 = Console.ReadLine();
+            Console.Write("Enter Max Marks: ");
+            student.MaxMarks = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Mark3: ");
-            stud.Mark3 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter Maximum Mark: ");
-            stud.MaxMark3 = int.Parse(Console.ReadLine());
-
-            return stud;
-
+            return student;
         }
     }
 }
