353f1b24e8c5ed71cb2c6d6aaa3b6fe6598e5907
devi-priya22 <sdevipriya803@gmail.com>
2024-10-17 08:25:46 +0530

    committed

---
+++ Day12/StudentInheritance.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInheritance
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    class SchoolStudent : Student
+    {
+        public string SchoolName { get; set; }
+
+        public override string ToString() => $"Name : {Name}, Email: {Email}, School: {SchoolName}";
+    }
+
+    class CollegeStudent : SchoolStudent
+    {
+        public string CollegeName { get; set; }
+
+        public override string ToString() => $"Name : {Name}, Email: {Email}, School: {SchoolName}, CollegeName: {CollegeName}";
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Student();
+            var cs = new CollegeStudent
+            {
+                Name = "Abcd",
+                Email = "Abcd@gmail.com",
+                SchoolName = "Abcde School",
+                CollegeName = " Abcdef College"
+            };
+            Console.WriteLine(cs);
+        }
+    }
+}
